f35cc96540389776e33c65c253c4f60ae63bc750
Jonghyun Kim [sc23j3k] <sc23j3k@uol-pc-055688.leeds.ac.uk>
2026-08-07 14:50:19 +0100

    implemented single control with both arm

--- bimanual_ws/src/task_node/scripts/task_node.py
+++ bimanual_ws/src/task_node/scripts/task_node.py
@@ -2,7 +2,21 @@
 import rospy
 from geometry_msgs.msg import PoseStamped
 
-def target_pose(x, y, z, qx=0.0, qy=0.0, qz=0.0, qw=1.0):
+HOME_QUAT = dict(qx=0.9239557, qy=-0.3824995, qz=0.0, qw=0.0)
+
+
+def target_pose(x, y, z, qx=None, qy=None, qz=None, qw=None):
+
+    if qx is None:
+        qx = HOME_QUAT["qx"]
+    if qy is None:
+        qy = HOME_QUAT["qy"]
+    if qz is None:
+        qz = HOME_QUAT["qz"]
+    if qw is None:
+        qw = HOME_QUAT["qw"]
+
+
     msg = PoseStamped()
     msg.header.stamp = rospy.Time.now()
     msg.header.frame_id = ""
@@ -15,21 +29,23 @@ def target_pose(x, y, z, qx=0.0, qy=0.0, qz=0.0, qw=1.0):
     msg.pose.orientation.w = qw
     return msg
 
+
 def main():
     rospy.init_node("task_node")
-    pub_left = rospy.Publisher("target_pose_left", PoseStamped, queue_size=10)
     pub_right = rospy.Publisher("target_pose_right", PoseStamped, queue_size=10)
+    pub_left = rospy.Publisher("target_pose_left", PoseStamped, queue_size=10)
 
     rospy.sleep(1.0)
+    right_target = target_pose(0.45, 0.3, 0.3, qx=-0.7071, qy=0.0, qz=0.0, qw=0.7071)
 
-    left_target = target_pose(0.4, 0.1, 0.45)
-    right_target = target_pose(0.4, -0.5, 0.45)
-
-    pub_left.publish(left_target)
-    rospy.loginfo("Published left target pose")
+    left_target = target_pose(0.45, -0.3, 0.5)
 
     pub_right.publish(right_target)
-    rospy.loginfo("Published right target pose")
+
+    rospy.sleep(5.0)
+
+    pub_left.publish(left_target)
+
 
 
 if __name__ == "__main__":
